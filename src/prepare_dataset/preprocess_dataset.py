e5a696ecb610b103f0e1ba439325a8a347a8a22e
mustafa-a-alturki <mustafa.a.alturki@gmail.com>
2023-12-29 06:13:05 +0000

    preprocessing ocde

--- src/prepare_dataset/preprocess_dataset.py
+++ src/prepare_dataset/preprocess_dataset.py
@@ -1,45 +1,17 @@
 from datasets import load_dataset, concatenate_datasets, arrow_dataset, Dataset
-from transformers import AutoTokenizer, DataCollatorForTokenClassification
+from transformers import AutoTokenizer
 import numpy as np
 from typing import List, Union
 import logging
 import os
 import glob
 import wandb
+from itertools import chain
+from collections import Counter
 
 logger = logging.getLogger(__name__)
 
-def get_raw_datasets() -> arrow_dataset.Dataset:
-    # columns: book, text where book is the title and text is the content as a list of one string 
-    logger.info("Downloading dataset")
-    raw_datasets = load_dataset("tashkeela", split="train", download_mode="force_redownload")
-    logger.info("done downloading")
-    
-    sub_datasets = []
-    logger.info("Spliting book content into multiple strings")
-    for d in raw_datasets:
-        sub_datasets.append(_split_text(d))
-    raw_datasets = concatenate_datasets(sub_datasets)
-    # after spliting: columns: book, text where book is the title and text is the part of the content as a list of one string 
-    # each book was one element in the dataset, now each book is represented by multiple elements, each one with same book value
-    # but text value will be a list of one string representing part of the content
-    return raw_datasets
-
-def _split_text(examples: arrow_dataset.Dataset) -> arrow_dataset.Dataset:
-    """
-    Receives a Dataset object where columns are book and text, book is the book title and text is the content as a list of one string.
-    It splits the content into a list of multiple strings each of size config.text_chunk_size.
-    """
-    result = {"book": [], "text": []}
-    words =  examples["text"].split()
-    sentences = [" ".join(words[i : i + config.text_chunk_size]) for i in range(0, len(words), config.text_chunk_size)]
-    for sent in sentences:
-        result["book"].append(examples["book"])
-        result["text"].append(sent)
-    examples = Dataset.from_dict(result)
-    return examples
-
-def _create_labels(examples: List[str], marks: str) -> List[List[str]]:
+def _create_labels(examples: List[str], marks: str, label2id) -> List[List[str]]:
     """
     Create labels at the word level (words are whatever separated by space), 
     where each word will have a label indicating the punctuation mark that 
@@ -78,6 +50,7 @@ def _remove_punc(examples: List[str], marks: str) -> List[str]:
     return new_examples
 
 def align_labels_with_tokens(labels:List[str], word_ids: List[Union[int, str]], input_ids: List[int]) -> List[int] :
+    # TODO may the label should be assigned to the last token instead of first
     """
     Make label list aligned with the tokens list. 
     Special tokens get a label of -100, to be ignored by the loss function.
@@ -97,13 +70,18 @@ def align_labels_with_tokens(labels:List[str], word_ids: List[Union[int, str]], 
             new_labels.append(-100)
     return new_labels
 
-def _tokenize_and_align_labels(examples: arrow_dataset.Dataset): # returns transformers.tokenization_utils_base.BatchEncoding (a dict subclass)  
-    all_labels: List[List[int]] = _create_labels(examples["text"], config.marks)
+def _tokenize_and_align_labels(examples: list[str], tokenizer, label2id, config):
+    """ returns transformers.tokenization_utils_base.BatchEncoding (a dict subclass)  
+    """
+    all_labels: List[List[int]] = _create_labels(examples, config["marks"], label2id)
 
     # remove marks from text before tokenization
-    examples["text"] = _remove_punc(examples["text"], config.marks)
-    tokenized_inputs = tokenizer(examples["text"]) # tokenized_inputs is transformers.tokenization_utils_base.BatchEncoding  (a dict subclass) with keys dict_keys(['input_ids', 'attention_mask'])
+    examples = _remove_punc(examples, config.marks)
+
+    # tokenized_inputs is transformers.tokenization_utils_base.BatchEncoding  (a dict subclass) 
+    # with keys dict_keys(['input_ids', 'attention_mask'])
     # the value of each key in tokenized_inputs is a List[List[int]] where each sub list represent one point in the dataset 
+    tokenized_inputs = tokenizer(examples, add_special_tokens=False)
     
     # align labels with tokens
     new_labels = []
@@ -115,77 +93,174 @@ def _tokenize_and_align_labels(examples: arrow_dataset.Dataset): # returns trans
     tokenized_inputs["word_ids"] = [tokenized_inputs.word_ids(i) for i in range(len(tokenized_inputs["input_ids"]))]
     return tokenized_inputs
 
+def _group_samples(tokenized_inputs, original, config) -> list[list[int]]:
+    max_length = config["tokens_max_len"]-2
+    combined_lists_dict = {"input_ids": [], "word_ids": [], "labels": [], "attention_mask": []}
+    combined_original = []
+    curr_combined_input_ids_list = []
+    start_index = 0  # Initialize the start index
 
-def _group_and_split_samples(examples) -> dict:
-    """
-    Create a dictionary where values are list of tokens of same size.
-    """
-    # Concatenate all texts 
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    # Compute length of concatenated texts
-    total_length = len(concatenated_examples[list(examples.keys())[-1]])
-    # drop the last chunk if it's smaller than chunk_size
-    total_length = (total_length // config.chunk_size) * config.chunk_size
-    # Split by chunks of max_len
-    result = {
-        k: [t[i : i + config.chunk_size] for i in range(0, total_length, config.chunk_size)]
-        for k, t in concatenated_examples.items()
-    }
-    return result
-
-def add_punct(decoded_str: str, word_ids: list, labels:list) -> str:
-    output = ""
-    words = decoded_str.split()
-    unique_word_ids = np.unique(word_ids)
-    labels = np.array(labels)
-    word_ids = np.array(word_ids)
-
-    assert len(words) == unique_word_ids.size
-    for word_id, word in zip(unique_word_ids, words):
-        punc_marks: np.ndarray = labels[word_ids==word_id]
-        # TODO need better aggregation later
-        if punc_marks[0] != -100 and id2label[punc_marks[0]] != "O":
-            output_punc_mark = id2label[punc_marks[0]]
+    # for i in enumerate(tokenized_inputs["input_ids"]):
+    for i, input_ids in enumerate(tokenized_inputs["input_ids"]):
+        if not curr_combined_input_ids_list:
+            # If the current combined list is empty, add the inner list
+            curr_combined_input_ids_list = input_ids
+        elif len(curr_combined_input_ids_list) + len(input_ids) <= max_length:
+            # If adding the inner list does not exceed the threshold, combine them
+            curr_combined_input_ids_list += input_ids
         else:
-            output_punc_mark = ""
-        output += word + output_punc_mark + " "
-    return output
+            # If adding the inner list exceeds the threshold, start a new combined list
+            end_index = i - 1  # Calculate the end index of the previous combined list
 
-def preprocess(list_file_names: List[str], src_data_home:str="clean", dst_data_home:str="processed") -> None:
+            combined_lists_dict["input_ids"].append(curr_combined_input_ids_list)         
+            # combined_lists_dict["word_ids"].append(list(chain(*tokenized_inputs["word_ids"][start_index:end_index+1])))
+            combined_lists_dict["labels"].append(list(chain(*tokenized_inputs["labels"][start_index:end_index+1])))
+            combined_lists_dict["attention_mask"].append(list(chain(*tokenized_inputs["attention_mask"][start_index:end_index+1])))
+            combined_original.append(" ".join(original[start_index:end_index+1]))
+
+            curr_combined_input_ids_list = input_ids
+            start_index = i  # Update the start index
+
+    # Add the last combined list (if any)
+    if curr_combined_input_ids_list:
+        end_index = len(tokenized_inputs["input_ids"]) - 1  # Calculate the end index for the last combined list
+
+        combined_lists_dict["input_ids"].append(curr_combined_input_ids_list)         
+        # combined_lists_dict["word_ids"].append(list(chain(*tokenized_inputs["word_ids"][start_index:end_index+1])))
+        combined_lists_dict["labels"].append(list(chain(*tokenized_inputs["labels"][start_index:end_index+1])))
+        combined_lists_dict["attention_mask"].append(list(chain(*tokenized_inputs["attention_mask"][start_index:end_index+1])))
+        combined_original.append(" ".join(original[start_index:end_index+1]))
+
+    # assert len(combined_original) == len(tokenized_inputs["input_ids"]), print(len(combined_original), len(tokenized_inputs["input_ids"]))
+    for key in tokenized_inputs.keys():
+        assert len(tokenized_inputs[key]) == len(tokenized_inputs["input_ids"]), f"problem with {key}"
+    return combined_lists_dict, combined_original
+
+def _tokens_labels_to_sent(input_ids: list[int], attention_mask, word_ids, labels, tokenizer, id2label):
+    labels = np.array(labels)
+    input_ids = np.array(input_ids)
+    word_ids = np.array(word_ids)
+    assert np.all(attention_mask), "Not all elements are True"
+    # attention_mask = np.array(attention_mask, dtype=bool)
+    # input_ids = input_ids[attention_mask]
+    # word_ids = word_ids[attention_mask]
+
+    labels = labels[labels!=-100]
+    # if len([id for id in word_ids if id is None]) != 0:
+    #     print(word_ids)
+    #     print(attention_mask)
+    #     print(labels)
+    #     print(tokenizer.decode(input_ids))
+    #     exit()
+    assert len(labels) == np.max([id for id in word_ids]) + 1
+
+    decoded_text = ""
+    for i in range(len(labels)):
+        word_input_ids = input_ids[word_ids == i]
+        decoded_text += tokenizer.decode(word_input_ids)
+        label = id2label[labels[i]]
+        if label != "O":
+            decoded_text += " " + label
+        decoded_text += " "
+    return decoded_text
+
+def _test_by_reconstruct(original, tokenized_inputs, tokenizer, id2label):
+    for key in tokenized_inputs.keys():
+        assert len(original) == len(tokenized_inputs[key])
+
+    reconstructed = []
+    for input_ids, attention_mask, word_ids, labels in zip(tokenized_inputs["input_ids"],
+                                                                         tokenized_inputs["attention_mask"],
+                                                                         tokenized_inputs["word_ids"],
+                                                                         tokenized_inputs["labels"]):
+        sent: str = _tokens_labels_to_sent(input_ids, attention_mask, word_ids, labels, tokenizer, id2label)
+        reconstructed.append(sent)
+    return reconstructed
+
+
+def preprocess(config, list_file_names: List[str], label2id, tokenizer, id2label) -> None:
     """
     The main preprocessing function which will call all necessary functions to produce a data set ready
     to be consumed by the training script
     """
+    global_counter = Counter()
     for file in list_file_names:
-        src_file_path = os.path.join(data_root, src_data_home, file)
-        dst_file_path = os.path.join(data_root, dst_data_home, file)
+        src_file_path = os.path.join(config["preprocess"]["input_path"], file)
+        dst_file_path = os.path.join(config["preprocess"]["output_path"], file)
+
         with open(src_file_path, "r") as in_file:
             lines = in_file.readlines()
             results = []
+            # divide the files by sentences ending in full stop
             for line in lines:
-                results += [l.strip()+".\n" for l in line.strip().split(".") if len(l)>0]
-        with open(dst_file_path, "w") as out_file:
-            out_file.writelines(results)
+                # results += [l.strip()+".\n" for l in line.strip().split(".") if len(l.strip())>0]
+                results += [l.strip()+".\n" for l in line.strip().split(".") 
+                            if len(_remove_punc([l.strip()], config["marks"])[0])>0]
 
-    
+            # tokenize
+            tokenized_inputs = _tokenize_and_align_labels(results, tokenizer, label2id, config)
+            reconstructed = _test_by_reconstruct(results, tokenized_inputs, tokenizer, id2label)
+
+            tokenized_inputs, results = _group_samples(tokenized_inputs, results, config)
+
+            print("second tokenization")
+            tokenized_inputs = _tokenize_and_align_labels(results, tokenizer, label2id, config)
+            reconstructed = _test_by_reconstruct(results, tokenized_inputs, tokenizer, id2label)
+
+
+        
+  
+        
+        # with open(dst_file_path, "w") as out_file:
+        #     for inner_list in reconstructed:
+        #         line = str(inner_list)
+        #         out_file.write(f"{line}\n")
+
+        # with open(dst_file_path, "w") as out_file:
+        #     for inner_list in tokenized:
+        #         line = str(inner_list)
+        #         out_file.write(f"{line}\n")
+   
 def go(config):
-    run = wandb.init(job_type="preprocessing")
-    run.config.update(config)
+    # run = wandb.init(job_type="preprocess_data")
+    # run.config.update(config)
+
 
     tokenizer = AutoTokenizer.from_pretrained(config["transformers_checkpoint"])
-    data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+    # data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
     id2label = {i: label for i, label in enumerate(config["marks"]+"O")}
     label2id = {v: k for k, v in id2label.items()}
     label2name = {k: config["mark2name"][k] for k in config["marks"]+"O"}
 
 
-
-    logger.info("Preprocessing dataset ...")
     list_file_names = [file_name.split("/")[-1] for file_name in 
-                glob.glob(os.path.join(config["input_path"], "book.txt"))]
-    preprocess(list_file_names)
+                glob.glob(os.path.join(config["preprocess"]["input_path"], "*.txt"))]
 
-# see main.py for passing arguments value through mlflow run for an entry point in the project. 
+
+
+
+    logger.info("Preprocessing dataset")
+    preprocess(config, list_file_names, label2id, tokenizer, id2label)
+    logger.info("Preprocessing is done")
+
+
+
+
+   
+    
+
+    logger.info(f"Uploading a sample file from {config['preprocess']['output_path']} to Weights & Biases")
+
+    # artifact = wandb.Artifact(
+    #     "sample_raw_data.txt",
+    #     type="raw data",
+    #     description="A sample raw data file which represent one book",
+    # )
+    # artifact.add_file(os.path.join(config['download']['output_path'], "01_أحكام القرآن لابن العربي.txt"))
+    # artifact.add_file(os.path.join(config['preprocess']['output_path'], "book.txt"))
+
+    # run.log_artifact(artifact)
+
 if __name__ == "__main__":
-    print("hello from preprocess")
+    pass
     # go(config)
