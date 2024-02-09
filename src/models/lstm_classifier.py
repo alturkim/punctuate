81211266619876c3acef711e146868d7da5c7ded
mustafa-a-alturki <mustafa.a.alturki@gmail.com>
2024-02-09 08:25:33 +0000

    preparing labels for the hierarical classifier, and fixing ignore_index for loss functions

--- src/models/lstm_classifier.py
+++ src/models/lstm_classifier.py
@@ -1,16 +1,18 @@
 """
 A base model that use a BiLSTM with Hierarchical classifier 
 """
-
+import torch
 from torch import nn
 from transformers import AutoModel
 
 class LSTMClassifier(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.config = config
         self.num_labels = len(config["marks"]) + 1
         self.checkpoint = config["train"]["transformers_checkpoint"]
         self.pretrained = AutoModel.from_pretrained(self.checkpoint)
+        self.hier_ignore_index = config["train"]["hier_ignore_index"]
         for param in self.pretrained.parameters():
             param.requires_grad = False
 
@@ -18,8 +20,8 @@ class LSTMClassifier(nn.Module):
         self.linear_hidden = config["train"]["lstm_cls"]["linear_hidden"] 
         self.embeddings_size = config["train"]["lstm_cls"]["embed_size"] 
 
-        self.binary_criterion = nn.BCEWithLogitsLoss()
-        self.multiclass_criterion = nn.CrossEntropyLoss(ignore_index=config["train"]["hier_ignore_index"])
+        self.binary_criterion = nn.BCEWithLogitsLoss(ignore_index=-100)
+        self.multiclass_criterion = nn.CrossEntropyLoss(ignore_index=-100) 
 
         self.embeddings = self.pretrained.embeddings.word_embeddings
         self.bilstm = nn.LSTM(
@@ -51,8 +53,22 @@ class LSTMClassifier(nn.Module):
         output_multiclass = self.fc_multiclass(output)
         multiclass_logits = self.fc_multiclass_out(output_multiclass)
 
-        binary_loss = self.binary_criterion(binary_logits)
-        multiclass_loss = self.multiclass_criterion(multiclass_logits)
+        # true for punc labels, false for no punc and -100 (pad token labels)        
+        mask = (torch.logical_and(labels != self.hier_ignore_index, labels != -100))
+        # true for pad token labels, false otherwise
+        mask_neg100 = labels == -100
+        # true for no punc label, false other wise
+        mask_no_punc = labels == 5
+
+        # multiclass_labels, same as labels, except that no punc label is replaced with -100
+        # so it is ignored by the multiclass loss. Note: adding labels*mask_neg100 is to keep
+        # the original pad token labels -100
+        multiclass_labels = labels*mask + labels*mask_neg100 + -100*mask_no_punc
+        # binary_labels are zero for no punc
+        binary_labels = mask.int() + labels*mask_neg100
+        
+        binary_loss = self.binary_criterion(binary_logits, binary_labels)
+        multiclass_loss = self.multiclass_criterion(multiclass_logits, multiclass_labels)
 
         loss = binary_loss + multiclass_loss
         return loss, binary_logits, multiclass_logits
