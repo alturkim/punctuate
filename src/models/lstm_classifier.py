7781bb06fc8c4ff1af0152b19ba16d499f6f7a5a
alturkim <mustafa.a.alturki@gmail.com>
2024-02-20 04:00:22 -0800

    new base model, weighted loss

--- src/models/lstm_classifier.py
+++ src/models/lstm_classifier.py
@@ -19,19 +19,40 @@ class LSTMClassifier(nn.Module):
         self.embeddings_size = config["train"]["lstm_cls"]["embed_size"] 
 
         self.embeddings = self.pretrained.embeddings.word_embeddings
+        # self.embeddings = nn.Embedding(30000, self.embeddings_size)
+
         self.bilstm = nn.LSTM(
             input_size=self.embeddings_size,
             hidden_size=self.lstm_hidden,
             num_layers=config["train"]["lstm_cls"]["lstm_layers"] , 
             batch_first=True, 
             bidirectional=True)
-        self.classifier_net = ClassifierNet(2*self.lstm_hidden, self.config)
+        self.hier_ignore_index = config["train"]["hier_ignore_index"] 
+        self.num_labels = len(config["marks"]) + 1
+        self.mc_linear_hidden_1 = config["train"]["classifier_net"]["mc_linear_hidden_1"]
+        self.mc_linear_hidden_2 = config["train"]["classifier_net"]["mc_linear_hidden_2"]
+
+
+        self.multiclass_criterion = nn.CrossEntropyLoss(
+            weight=torch.tensor([0.7432966375816064, 0.02141450301537206, 0.16100701881706023, 0.04058612682252735, 0.03277620324212341, 0.0009195105213105526]),
+            ignore_index=-100) 
+
+        # Multiclass classification layer
+        self.fc_multiclass_1 = nn.Linear(2*self.lstm_hidden, self.mc_linear_hidden_1)
+        self.fc_multiclass_2 = nn.Linear(self.mc_linear_hidden_1, self.mc_linear_hidden_2)
+        self.fc_multiclass_out = nn.Linear(self.mc_linear_hidden_2, self.num_labels)
 
     def forward(self, input_ids=None, labels=None):
         # shape [batch_size, seq_len, embeddings_size]
         output = self.embeddings(input_ids)
         # shape [batch_size, seq_len, 2*lstm_hidden]
         output, _ = self.bilstm(output)
-        loss, binary_logits, multiclass_logits = self.classifier_net(output, labels)
-        assert multiclass_logits.shape[1] == labels.shape[1]
-        return loss, binary_logits, multiclass_logits
+        
+
+        output_multiclass_1 = torch.tanh(self.fc_multiclass_1(output))
+        output_multiclass_2 = self.fc_multiclass_2(output_multiclass_1)
+
+        logits = self.fc_multiclass_out(output_multiclass_2)
+
+        loss = self.multiclass_criterion(logits.permute(0, 2, 1), labels)
+        return loss, logits
